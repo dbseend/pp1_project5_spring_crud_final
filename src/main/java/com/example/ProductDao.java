e55f969e39cde9e6ba7ae506ea0780bfbcf97d3a
dbseend <141024382+dbseend@users.noreply.github.com>
2023-12-15 17:59:19 +0900

    상품 추가 기능 구현 (#4)
    * 세팅 수정
    
    * 상품 추가 기능 구현

--- src/main/java/com/example/ProductDao.java
+++ src/main/java/com/example/ProductDao.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -11,9 +12,14 @@ public class ProductDao {
 
     @Autowired
     SqlSession sqlSession;
+
     public int insertProduct(ProductVO vo) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        vo.setItemTime(currentTime);
+        vo.setRegDate(currentTime);
         return sqlSession.insert("Product.insertProduct", vo);
     }
+
     public int updateProduct(ProductVO vo) {
         return sqlSession.update("Product.updateProduct", vo);
     }
@@ -22,12 +28,15 @@ public class ProductDao {
     public List<ProductVO> getProducts() {
         return sqlSession.selectList("Product.getProductList");
     }
+
     public ProductVO getProduct(Integer id) {
         return sqlSession.selectOne("Product.getProduct", id);
     }
+
     public void deleteProduct(Integer id) {
         sqlSession.delete("Product.deleteProduct", id);
     }
+
     public List<ProductVO> getProductsByProductName(String keyword) {
         return sqlSession.selectList("Product.getProductListByProductName", "%" + keyword + "%");
     }
